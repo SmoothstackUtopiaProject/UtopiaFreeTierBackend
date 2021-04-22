982cc2930496e17c143a78babbae0a93c1637378
JoshuaTyler-SmoothStack <Joshua.Tyler@smoothstack.com>
2021-04-22 15:54:50 -0500

    Hotfix: JPA table rename to "payments"

--- src/main/java/com/ss/utopia/repositories/PaymentRepository.java
+++ src/main/java/com/ss/utopia/repositories/PaymentRepository.java
@@ -12,12 +12,12 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface PaymentRepository extends JpaRepository<Payment, Integer> {
 
-  @Query(value = "SELECT * FROM payment WHERE booking_uuid = ?1", nativeQuery = true)
+  @Query(value = "SELECT * FROM payments WHERE booking_uuid = ?1", nativeQuery = true)
   Optional<Payment> findByBookingUuid(String bookingUuid);
 
-  @Query(value = "SELECT * FROM payment WHERE stripe_uuid = ?1", nativeQuery = true)
+  @Query(value = "SELECT * FROM payments WHERE stripe_uuid = ?1", nativeQuery = true)
   Optional<Payment> findByStripeUuid(String stripeUuid);
 
-  @Query(value = "SELECT * FROM payment WHERE status = ?1", nativeQuery = true)
+  @Query(value = "SELECT * FROM payments WHERE status = ?1", nativeQuery = true)
   List<Payment> findAllWithStatus(String status);
 }
