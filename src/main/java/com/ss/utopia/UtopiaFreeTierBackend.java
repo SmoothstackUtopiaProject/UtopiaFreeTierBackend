7aa1ea8998b017e2f055a06107285a8644076bf7
JoshuaTyler-SmoothStack <77794388+JoshuaTyler-SmoothStack@users.noreply.github.com>
2021-05-03 12:11:47 -0500

    Update UtopiaFreeTierBackend.java

--- src/main/java/com/ss/utopia/UtopiaFreeTierBackend.java
+++ src/main/java/com/ss/utopia/UtopiaFreeTierBackend.java
@@ -13,12 +13,13 @@ public class UtopiaFreeTierBackend {
   private static final String STRIPE_API_KEY = "STRIPE_API_KEY";
 
   public static void main(String[] args) {
+    
+    // Environment Variables
     Map<String, String> env = System.getenv();
     for (String envName : env.keySet()) {
+      
       // Stripe
       if(envName.equals(STRIPE_API_KEY)) {
-        System.out.println("===================== I GOT IT! ====================");
-        System.out.println(env.get(STRIPE_API_KEY));
         Stripe.apiKey = env.get(STRIPE_API_KEY);
       }
     }
