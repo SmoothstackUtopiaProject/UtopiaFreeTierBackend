14d8e9d1b6e6271dd06351825db569d37463ec77
JoshuaTyler-SmoothStack <77794388+JoshuaTyler-SmoothStack@users.noreply.github.com>
2021-05-04 14:46:28 -0500

    Hotfix: Exception Handling Error Messages

--- src/main/java/com/ss/utopia/controllers/ExceptionHandlerControllerAdvice.java
+++ src/main/java/com/ss/utopia/controllers/ExceptionHandlerControllerAdvice.java
@@ -5,6 +5,8 @@ import com.ss.utopia.exceptions.TokenAlreadyIssuedException;
 import com.ss.utopia.exceptions.TokenNotFoundExpection;
 import com.ss.utopia.exceptions.UserAlreadyExistsException;
 import com.ss.utopia.exceptions.UserNotFoundException;
+import com.ss.utopia.models.ErrorMessage;
+
 import java.net.ConnectException;
 import java.sql.SQLException;
 import org.springframework.http.HttpStatus;
@@ -20,38 +22,53 @@ public class ExceptionHandlerControllerAdvice {
   @ExceptionHandler(UserAlreadyExistsException.class)
   @ResponseStatus(HttpStatus.CONFLICT)
   public ResponseEntity<Object> userAlreadyExistsException(Throwable err) {
-    return new ResponseEntity<>(err.getMessage(), HttpStatus.CONFLICT);
+    return new ResponseEntity<>(
+      new ErrorMessage(err.getMessage()),
+      HttpStatus.CONFLICT
+    );
   }
 
   @ExceptionHandler(TokenAlreadyIssuedException.class)
   @ResponseStatus(HttpStatus.CONFLICT)
   public ResponseEntity<Object> tokenAlreadyExistsException(Throwable err) {
-    return new ResponseEntity<>(err.getMessage(), HttpStatus.CONFLICT);
+    return new ResponseEntity<>(
+      new ErrorMessage(err.getMessage()),
+      HttpStatus.CONFLICT
+    );
   }
 
   @ExceptionHandler(UserNotFoundException.class)
   @ResponseStatus(HttpStatus.NOT_FOUND)
   public ResponseEntity<Object> userNotFoundException(Throwable err) {
-    return new ResponseEntity<>(err.getMessage(), HttpStatus.NOT_FOUND);
+    return new ResponseEntity<>(
+      new ErrorMessage(err.getMessage()),
+      HttpStatus.NOT_FOUND
+    );
   }
 
   @ExceptionHandler(TokenNotFoundExpection.class)
   @ResponseStatus(HttpStatus.NOT_FOUND)
   public ResponseEntity<Object> tokenNotFoundExeption(Throwable err) {
-    return new ResponseEntity<>(err.getMessage(), HttpStatus.NOT_FOUND);
+    return new ResponseEntity<>(
+      new ErrorMessage(err.getMessage()),
+      HttpStatus.NOT_FOUND
+    );
   }
 
   @ExceptionHandler(ExpiredTokenExpception.class)
   @ResponseStatus(HttpStatus.NOT_FOUND)
   public ResponseEntity<Object> tokenNotFoundException(Throwable err) {
-    return new ResponseEntity<>(err.getMessage(), HttpStatus.NOT_FOUND);
+    return new ResponseEntity<>(
+      new ErrorMessage(err.getMessage()),
+      HttpStatus.NOT_FOUND
+    );
   }
 
   @ExceptionHandler(HttpMessageNotReadableException.class)
   @ResponseStatus(HttpStatus.BAD_REQUEST)
   public ResponseEntity<Object> invalidMessage() {
     return new ResponseEntity<>(
-      "Invalid HTTP message content",
+      new ErrorMessage("Invalid HTTP message content"),
       HttpStatus.BAD_REQUEST
     );
   }
@@ -59,14 +76,17 @@ public class ExceptionHandlerControllerAdvice {
   @ExceptionHandler(IllegalArgumentException.class)
   @ResponseStatus(HttpStatus.BAD_REQUEST)
   public ResponseEntity<Object> illegalArgumentException(Throwable err) {
-    return new ResponseEntity<>(err.getMessage(), HttpStatus.BAD_REQUEST);
+    return new ResponseEntity<>(
+      new ErrorMessage(err.getMessage()),
+      HttpStatus.BAD_REQUEST
+    );
   }
 
   @ExceptionHandler(SQLException.class)
   @ResponseStatus(HttpStatus.SERVICE_UNAVAILABLE)
   public ResponseEntity<Object> sqlException() {
     return new ResponseEntity<>(
-      "Service temporarily unavailabe.",
+      new ErrorMessage("Service temporarily unavailabe."),
       HttpStatus.SERVICE_UNAVAILABLE
     );
   }
@@ -75,7 +95,7 @@ public class ExceptionHandlerControllerAdvice {
   @ResponseStatus(HttpStatus.SERVICE_UNAVAILABLE)
   public ResponseEntity<Object> invalidConnection() {
     return new ResponseEntity<>(
-      "Service temporarily unavailabe.",
+      new ErrorMessage("Service temporarily unavailabe."),
       HttpStatus.SERVICE_UNAVAILABLE
     );
   }
